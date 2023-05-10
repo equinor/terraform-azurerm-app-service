e288a34218c2d76a43009d37e890cd564ef86ec9
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2023-05-10 10:24:36 +0200

    feat: enable autoscaling

--- main.tf
+++ main.tf
@@ -7,3 +7,20 @@ resource "azurerm_service_plan" "this" {
 
   tags = var.tags
 }
+
+resource "azurerm_monitor_autoscale_setting" "this" {
+  name                = "app-service-autoscaling"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  target_resource_id  = azurerm_service_plan.this.id
+  enabled             = var.autoscale_enabled
+
+  profile {
+    name = "app-autoscaling-profile"
+    capacity {
+      default = var.autoscale_profile_capacity_default
+      minimum = var.autoscale_profile_capacity_minimum
+      maximum = var.autoscale_profile_capacity_maximum
+    }
+  }
+}
