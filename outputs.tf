91785de424f95385f3983c1ae46e7e0a8be11a29
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-07-24 09:34:12 +0200

    feat: output OS type

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "kind" {
   value       = azurerm_service_plan.this.kind
 }
 
+output "os_type" {
+  description = "The OS type of this App Service plan."
+  value       = azurerm_service_plan.this.os_type
+}
+
 output "reserved" {
   description = "Is this a reserved App Service plan type?"
   value       = azurerm_service_plan.this.reserved
