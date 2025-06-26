b38574072e951e7e81b51d9c596d4a212ab115c6
Stian Rossland <94676511+stiross79@users.noreply.github.com>
2025-06-26 08:45:56 +0200

    feat: add complete example for App Service module with monitoring and alerts

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -6,19 +6,6 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
-resource "azurerm_monitor_action_group" "this" {
-  name                = "ag-${random_id.example.hex}"
-  resource_group_name = var.resource_group_name
-  short_name          = "p0action"
-  enabled             = true
-
-  arm_role_receiver {
-    name                    = "Monitoring Contributor"
-    role_id                 = "749f88d5-cbae-40b8-bcfc-e573ddc772fa"
-    use_common_alert_schema = true
-  }
-}
-
 module "app_service" {
   # source = "equinor/app-service/azurerm"
   source = "../.."
@@ -26,15 +13,4 @@ module "app_service" {
   plan_name           = "plan-${random_id.example.hex}"
   resource_group_name = var.resource_group_name
   location            = var.location
-}
-
-module "app_service_alerts" {
-  # source  = "equinor/app-service/azurerm//modules/alerts"
-  source  = "../../modules/alerts"
-  version = "~> 2.1"
-
-  resource_group_name = azurerm_resource_group.example.name
-  plan_name           = module.app_service.plan_name
-  plan_id             = module.app_service.plan_id
-  action_group_id     = azurerm_monitor_action_group.example.id
 }
